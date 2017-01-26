61cb02946b99f14841adf28da6111f7db7329096
addshore <addshorewiki@gmail.com>
2017-01-26 16:06:38 +0100

    Initial Commit
                       ____
                      (_  _)
            .  .       / /
         .`_._'_..    / /
         \   o   /   / /
          \ /   /  _/ /_
    `. ~. `\___/'./~.' /.~'`.
    .`'`.`.'`'`.~.`'~.`'`.~`
    
    Change-Id: I6e89c87d65179870e812b81575c8732d513b5b26

---
+++ Hooks.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace MoveToCommonsClient;
+
+use Message;
+use SkinTemplate;
+
+/**
+ * @author Addshore
+ */
+class MoveToCommonsClientHooks {
+
+	public static function onSkinTemplateNavigation( SkinTemplate &$sktemplate, array &$links ) {
+		global $wgMoveToCommonsClientTarget;
+
+		if ( $sktemplate->getTitle()->getNamespace() !== NS_FILE ) {
+			return;
+		}
+
+		$links['views']['moveToCommons'] = [
+			'class' => '',
+			'text' => Message::newFromKey( 'moveToCommonsClient-text' )->plain(),
+			'href' => $wgMoveToCommonsClientTarget,
+		];
+	}
+
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# MoveToCommonsClient extension
