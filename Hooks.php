f019db26c8e806fd51c83a09422e145f0cc6ccd1
addshore <addshorewiki@gmail.com>
2017-03-23 09:54:58 +0000

    Only show link for local files
    Bug: T161126
    Change-Id: I7c8ba2a43cee4b54fa5026fcbb945c7e3fd3c8af

--- Hooks.php
+++ Hooks.php
@@ -4,6 +4,7 @@ namespace FileExporter;
 
 use Message;
 use SkinTemplate;
+use WikiFilePage;
 
 /**
  * @author Addshore
@@ -13,8 +14,12 @@ class FileExporterHooks {
 	public static function onSkinTemplateNavigation( SkinTemplate &$sktemplate, array &$links ) {
 		global $wgFileExporterTarget;
 
+		$title = $sktemplate->getTitle();
+		$page = new WikiFilePage( $title );
+
 		if (
-			$sktemplate->getTitle()->getNamespace() !== NS_FILE ||
+			$title->getNamespace() !== NS_FILE ||
+			!$page->isLocal() ||
 			$sktemplate->getUser()->isNewbie()
 		) {
 			return;
